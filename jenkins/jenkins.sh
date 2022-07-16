1df1f7928eeaf64322f4b9a776a586d3a5a80819
ark4dii <ark4dii@MacBook-Pro.local>
2022-07-16 16:10:57 +0300

    Jenkins Dockerfile

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -2,3 +2,5 @@
 mkdir /opt/jenkins
 apt-get -y install apt-utils bind9-utils git sshpass tini daemon net-tools psmisc
 apt-get --no-install-recommends install -q -y openjdk-11-jre-headless
+apt-get autoremove -y
+apt-get autoclean -y
--- kibana/kibana.sh
+++ kibana/kibana.sh
@@ -2,3 +2,5 @@
 mkdir /opt/kibana
 tar -xvf kibana-8.2.1-linux-aarch64.tar.gz -C kibana --strip-components=1
 rm kibana-8.2.1-linux-aarch64.tar.gz
+apt-get autoremove -y
+apt-get autoclean -y
