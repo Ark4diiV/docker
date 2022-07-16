1d0c74cbb056e019613819fed5cb5f112309773f
ark4dii <ark4dii@MacBook-Pro.local>
2022-07-16 12:27:50 +0300

    Kibana Dockerfile

--- kibana/kibana.sh
+++ kibana/kibana.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir /opt/kibana
-tar -xf kibana-8.2.1-linux-aarch64.tar.gz -C kibana --strip-components=1
+tar -xvf kibana-8.2.1-linux-aarch64.tar.gz -C kibana --strip-components=1
 rm kibana-8.2.1-linux-aarch64.tar.gz
